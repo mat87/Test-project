6c13c7921210064a89bfbecd54bfe6b8e755c9ad
mat87 <mateusz.gradzik@gmail.com>
2016-08-30 16:50:44 +0200

    fix changeableContent in case of refresh page

--- coffee/script.coffee
+++ coffee/script.coffee
@@ -1,6 +1,13 @@
-# Add "Back to top" button on right side of page
+# Add "Back to top" button on bottom-right side of page
 $(window).ready ->
-	jQuery.goup location: 'right', containerSize: 60, trigger: 400, arrowColor: '#1fa67a', containerColor: '#333'
+  jQuery.goup location: 'right', containerSize: 60, trigger: 400, arrowColor: '#1fa67a', containerColor: '#333'
+# When F5 pressed or page has been reloaded <div> content is not missed
+$(document).ready ->
+   href = location.hash.slice(1)
+   if href is 'kontakt'
+     $('#changeableContent').load 'kontakt.html', ->
+       submitBtnOff()
+   else $('#changeableContent').load 'beers.html #' + href
 # Load <div> from external beers.html file in to <div> in index.html
 # Enables "Wyślij" buton when nick, email and message inputs have been fullfield
 # Otherwise it is disabled
@@ -8,10 +15,13 @@ $(window).on 'hashchange', ->
 	href = location.hash.slice(1)
 	switch href
 		when 'kontakt' then $('#changeableContent').load 'kontakt.html', ->
-				$('#submit-button').prop 'disabled', true
-				$('#name, #email, #message').on 'keyup', ->
-					if $('#name').val() is '' or $('#email').val() is '' or $('#message').val() is ''
-    				$('#submit-button').prop 'disabled', true
-  				else
-    				$('#submit-button').prop 'disabled', false
+		 submitBtnOff()
 		else $('#changeableContent').load 'beers.html #' + href
+
+submitBtnOff = ->
+  $('#submit-button').prop 'disabled', true
+  $('#name, #email, #message').on 'keyup', ->
+    if $('#name').val() is '' or $('#email').val() is '' or $('#message').val() is ''
+      $('#submit-button').prop 'disabled', true
+    else
+      $('#submit-button').prop 'disabled', false
--- coffee/script.js
+++ coffee/script.js
@@ -1,4 +1,6 @@
 (function() {
+  var submitBtnOff;
+
   $(window).ready(function() {
     return jQuery.goup({
       location: 'right',
@@ -9,24 +11,40 @@
     });
   });
 
+  $(document).ready(function() {
+    var href;
+    href = location.hash.slice(1);
+    if (href === 'kontakt') {
+      return $('#changeableContent').load('kontakt.html', function() {
+        return submitBtnOff();
+      });
+    } else {
+      return $('#changeableContent').load('beers.html #' + href);
+    }
+  });
+
   $(window).on('hashchange', function() {
     var href;
     href = location.hash.slice(1);
     switch (href) {
       case 'kontakt':
         return $('#changeableContent').load('kontakt.html', function() {
-          $('#submit-button').prop('disabled', true);
-          return $('#name, #email, #message').on('keyup', function() {
-            if ($('#name').val() === '' || $('#email').val() === '' || $('#message').val() === '') {
-              return $('#submit-button').prop('disabled', true);
-            } else {
-              return $('#submit-button').prop('disabled', false);
-            }
-          });
+          return submitBtnOff();
         });
       default:
         return $('#changeableContent').load('beers.html #' + href);
     }
   });
+
+  submitBtnOff = function() {
+    $('#submit-button').prop('disabled', true);
+    return $('#name, #email, #message').on('keyup', function() {
+      if ($('#name').val() === '' || $('#email').val() === '' || $('#message').val() === '') {
+        return $('#submit-button').prop('disabled', true);
+      } else {
+        return $('#submit-button').prop('disabled', false);
+      }
+    });
+  };
 
 }).call(this);
